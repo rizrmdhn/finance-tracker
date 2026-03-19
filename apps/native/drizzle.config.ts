db736560af3335c4eef5140e1238919ce510007b
Noor Rizki Ramadhan <rizrmdhn.work@gmail.com>
2026-03-19 20:48:52 +0800

    chore(native): update schema path in drizzle configuration
    - changed schema path to include all transaction schemas

--- apps/native/drizzle.config.ts
+++ apps/native/drizzle.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "../../packages/db/src/schema/transactions.ts",
-  out: "./drizzle",
-  dialect: "sqlite",
-  driver: "expo",
+	schema: "../../packages/db/src/schema/*",
+	out: "./drizzle",
+	dialect: "sqlite",
+	driver: "expo",
 });
