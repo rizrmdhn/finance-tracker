4b1df648a12027aeea2e2c5c5a041eb2e59211aa
Noor Rizki Ramadhan <rizrmdhn.work@gmail.com>
2026-03-21 09:12:38 +0800

    feat(updater): enhance auto-update functionality and IPC communication
    - add check for updates in development mode
    - send update notifications and progress to renderer process
    - expose additional update-related methods in preload script

--- apps/desktop/src/preload/index.ts
+++ apps/desktop/src/preload/index.ts
@@ -4,9 +4,25 @@ import { exposeElectronTRPC } from "trpc-electron/main";
 contextBridge.exposeInMainWorld("electronTRPC", exposeElectronTRPC());
 
 contextBridge.exposeInMainWorld("updater", {
-	onUpdateAvailable: (callback: () => void) =>
-		ipcRenderer.on("update-available", callback),
+	checkForUpdates: () => ipcRenderer.send("check-for-updates"),
+	onUpdateAvailable: (
+		callback: (info: { version: string; releaseNotes: string | null }) => void,
+	) => ipcRenderer.on("update-available", (_event, info) => callback(info)),
+	onUpdateNotAvailable: (callback: () => void) =>
+		ipcRenderer.on("update-not-available", () => callback()),
+	onDownloadProgress: (
+		callback: (progress: {
+			percent: number;
+			transferred: number;
+			total: number;
+		}) => void,
+	) =>
+		ipcRenderer.on("download-progress", (_event, progress) =>
+			callback(progress),
+		),
 	onUpdateDownloaded: (callback: () => void) =>
-		ipcRenderer.on("update-downloaded", callback),
+		ipcRenderer.on("update-downloaded", () => callback()),
 	installUpdate: () => ipcRenderer.send("install-update"),
+	removeAllListeners: (channel: string) =>
+		ipcRenderer.removeAllListeners(channel),
 });
